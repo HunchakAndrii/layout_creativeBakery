26856f6037a59df72f3cf6e0492f78fb67922d42
HunchakAndrii <andreygunchak1@gmail.com>
2024-03-22 12:38:29 +0200

    make build

---
+++ index.1a78d691.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".burger"),t=document.querySelector(".mobile-menu"),c=document.querySelectorAll(".nav__link--mobile");e.addEventListener("click",()=>{e.classList.toggle("active"),t.classList.toggle("active")}),c.forEach(c=>c.addEventListener("click",()=>{e.classList.remove("active"),t.classList.remove("active")}));
+//# sourceMappingURL=index.1a78d691.js.map
